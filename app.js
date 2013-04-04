aa0e38174eb9d8e32a82285d5c22659653441360
Gus Ireland <aireland@andrew.cmu.edu>
2013-04-04 19:33:50 -0400

    Mongodb things

--- app.js
+++ app.js
@@ -2,82 +2,67 @@
 
 var express = require("express");
 var app = express(); 
+var fs = require("fs");
+
 var mongo = require('mongodb');
 var host = 'localhost';
 var port = mongo.Connection.DEFAULT_PORT;
-var optionsWithEnableWriteAccess = { w: 1 };
 
-var dbName = 'db';
-var fs = require("fs");
+var optionsWithEnableWriteAccess = { w: 1 };
+var dbName = 'testDb';
+
+var client = new mongo.Db(
+    dbName,
+    new mongo.Server(host, port),
+    optionsWithEnableWriteAccess
+);
+
+function openDb(onOpen){
+    client.open(onDbReady);
+
+    function onDbReady(error){
+        if (error)
+            throw error;
+        client.collection('testCollection', onTestCollectionReady);
+    }
+
+    function onTestCollectionReady(error, testCollection){
+        if (error)
+            throw error;
+
+        onOpen(testCollection);
+    }
+}
+
+function closeDb(){
+    client.close();
+}
 
 app.use(express.bodyParser());
 app.use(express.static(__dirname + '/static/'));
 
-app.listen(5555, function(){
-	console.log("Express server listening on port " + 5555);
-});
 
 app.post("/create", function(request, response) {
-  var groupID = request.body.auth.group.id;
-  var user = request.body.auth.user;
-  var list = request.body.list;
+  var list = request.body.files;
   createDBEntry(list);
   response.send({
-    wallPosts: groupWallPosts,
     success: true
   });
 });
 
 function createDBEntry(list){
-	for(var i = 0; i < list.length; i++){
-		for(var j = 0; j < list[0].length; i++){
-		list[i][j];
-		tempObject = {'': v1};
-		}
+	var objectList = new Array();
+	for(var i = 1; i < list.length; i++){
+		name = list[i][1];
+		tempObject = {'name': name};
+		objectList.push(tempObject);
 	}
-	insertDocuments(objectList, dbName)
+	insertDocuments(donations, objectList)
 }
 
 
 
-var client = new mongo.Db(
-	dbName,
-	new mongo.Server(host, port),
-	optionsWithEnableWriteAccess
-	);
 
-function openDb(onOpen){
-	client.open(onDbReady);
-
-	function onDbReady(error){
-		if (error)
-			throw error;
-		client.collection('donations', onTestCollectionReady);
-	}
-
-	function onTestCollectionReady(error, testCollection){
-		if (error)
-			throw error;
-
-		onOpen(testCollection);
-	}
-}
-
-function closeDb(){
-	client.close();
-}
-
-function insertDocuments(collection, docs, done){
-	if (docs.length === 0){
-		done(null);
-		return;
-	}
-    var docHead = docs.shift(); //shift removes first element from docs
-    collection.insert(docHead, function onInserted(err){
-    	if (err){
-    		done(err);
-    		return;
-    	}
-    	insertDocuments(collection, docs, done);
-    });
-}
+app.listen(5555, function(){
+	console.log("Express server listening on port " + 5555);
+});
--- static/file_upload.js
+++ static/file_upload.js
@@ -19,8 +19,20 @@ function handleFileSelect(evt) {
             }
 
             console.log(array);
+            add(array);
         };
     })(f);
     reader.readAsText(f);
+
 }
 document.getElementById('files').addEventListener('change', handleFileSelect, false);
+
+  function add(files) {
+    $.ajax({
+      type: "post",
+      data: {"files": files},
+      url: "/create",
+      success: function(data) {
+           }
+    });
+  }
