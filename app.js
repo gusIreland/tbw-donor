4939b4bcccdb7c59ffeb54dd81fda8fb3c16d742
Gus Ireland <aireland@andrew.cmu.edu>
2013-04-05 15:35:16 -0400

    Added jquery, connects to mongo kinda

--- app.js
+++ app.js
@@ -3,19 +3,20 @@
 var express = require("express");
 var app = express(); 
 var fs = require("fs");
-
+var coll;
 var mongo = require('mongodb');
 var host = 'localhost';
 var port = mongo.Connection.DEFAULT_PORT;
 
 var optionsWithEnableWriteAccess = { w: 1 };
 var dbName = 'testDb';
-
+var collections;
 var client = new mongo.Db(
     dbName,
     new mongo.Server(host, port),
     optionsWithEnableWriteAccess
 );
+
 
 function openDb(onOpen){
     client.open(onDbReady);
@@ -29,14 +30,20 @@ function openDb(onOpen){
     function onTestCollectionReady(error, testCollection){
         if (error)
             throw error;
+        
+        testCollection.remove({}, function(error){
+            if (error)
+                throw error;
+            onOpen(testCollection);
+        });
 
-        onOpen(testCollection);
     }
 }
 
 function closeDb(){
     client.close();
 }
+openDb(onDbOpen);
 
 app.use(express.bodyParser());
 app.use(express.static(__dirname + '/static/'));
@@ -50,6 +57,10 @@ app.post("/create", function(request, response) {
   });
 });
 
+function onDbOpen(collection){
+collections = collection;
+}
+
 function createDBEntry(list){
 	var objectList = new Array();
 	for(var i = 1; i < list.length; i++){
@@ -57,9 +68,23 @@ function createDBEntry(list){
 		tempObject = {'name': name};
 		objectList.push(tempObject);
 	}
-	insertDocuments(donations, objectList)
+	insertDocuments('testCollection', objectList)
 }
 
+function insertDocuments(collection, docs, done){
+    if (docs.length === 0){
+        done(null);
+        return;
+    }
+    var docHead = docs.shift(); //shift removes first element from docs
+    dbName.testCollection.insert(docHead, function onInserted(err){
+        if (err){
+            done(err);
+            return;
+        }
+        insertDocuments(collection, docs, done);
+    });
+}
 
 
 
